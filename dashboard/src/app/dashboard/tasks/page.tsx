d98bd0f986b92004702ec8f257a04500fe53da98
NatnaelDemelash <natidemelash074@gmail.com>
2025-03-08 21:59:56 +0300

    style update for tasks page

--- dashboard/src/app/dashboard/tasks/page.tsx
+++ dashboard/src/app/dashboard/tasks/page.tsx
@@ -26,22 +26,39 @@ const Tasks = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-blue-50 p-8">
-      <div className="max-w-7xl mx-auto bg-white rounded-lg shadow-sm p-8">
-        <h1 className="text-3xl font-bold text-gray-800 mb-8">Tasks</h1>
+    <div className="min-h-screen bg-gradient-to-br from-gray-700 to-gray-800 p-8">
+      <div className="max-w-7xl mx-auto bg-gray-600 rounded-lg shadow-sm p-8">
+        <h1 className="text-3xl font-bold text-gray-100 mb-8">Tasks</h1>
 
         <section className="space-y-6">
           {tasks.map((task) => {
             return (
               <div
                 key={task.id}
-                className="flex items-center justify-between p-6 bg-gray-100 mb-6 text-gray-800 shadow-md border-l-4 rounded-lg"
+                className="flex items-center justify-between p-6 bg-gray-700  mb-6 text-white shadow-md border-l-4 rounded-lg"
               >
-                <div className="flex flex-col space-y-3">
-                  <h2> {task.title}</h2>
-                  <p>{task.describtion}</p>
-                  <p>{task.dueDate}</p>
+                <div className="flex flex-col space-y-2">
+                  <h2 className="text-xl font-semibold text-gray-100">
+                    {' '}
+                    {task.title}
+                  </h2>
+                  <p className="text-sm text-gray-200">{task.describtion}</p>
+                  <p className="text-sm text-gray-200">{task.dueDate}</p>
                 </div>
+
+                <section className="space-y-4">
+                  <span
+                    className={`px-4 py-3 rounded-full ${
+                      task.status === 'Completed'
+                        ? 'bg-green-200 text-green-800'
+                        : task.status === 'Inprogress'
+                        ? 'bg-yellow-200 text-yellow-900'
+                        : 'bg-red-200 text-red-950'
+                    }`}
+                  >
+                    {task.status}
+                  </span>
+                </section>
               </div>
             );
           })}
